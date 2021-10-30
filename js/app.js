7f2d36f23c9693fc98a3b0a858cac961b40232c0
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2021-10-30 17:16:10 +0500

    added firebase integration

--- js/app.js
+++ js/app.js
@@ -1,3 +1,45 @@
+
+
+    firebase.database().ref('/todo').on('child_added',function(element){
+        // console.log(element.val())
+        var data = element.val();
+       console.log(data)
+            
+        var li = document.createElement('li')
+    var textVal = document.createTextNode(data.todo);
+
+    li.appendChild(textVal);
+
+    var ulList = document.getElementById('todoList');
+    ulList.appendChild(li);
+
+
+    
+    var span = document.createElement('span')
+
+    var edtBtn = document.createElement('button')
+    var edtText = document.createTextNode('Edit')
+    edtBtn.setAttribute('onclick','edtbtn(this)');
+    edtBtn.setAttribute('id',data.key_item)
+    edtBtn.appendChild(edtText);
+    
+    span.appendChild(edtBtn)
+    
+    
+    var dltBtn = document.createElement('button')
+    var dltText = document.createTextNode('Delete')
+    dltBtn.setAttribute('onclick','dltbtn(this)');
+    dltBtn.setAttribute('id',data.key_item)
+
+    
+    dltBtn.appendChild(dltText);
+    span.appendChild(dltBtn)
+    // var val = inputField.value;
+    li.appendChild(span)
+
+    })
+
+
 function addTodo() {
 
     if(document.getElementById('inputTodo').value === ''){
@@ -8,38 +50,18 @@ function addTodo() {
 
         var inputField = document.getElementById('inputTodo');
         
-        
-    var li = document.createElement('li')
-    var textVal = document.createTextNode(inputField.value);
+    
+    var key = firebase.database().ref('/todo').push().getKey();
+    var obj = {
+        key_item : key,
+        todo : inputField.value
+    }
+    firebase.database().ref('/todo/'+key).set(obj)
 
-    li.appendChild(textVal);
 
-    var ulList = document.getElementById('todoList');
-    ulList.appendChild(li);
 
 
     inputField.value = '';
-
-    var span = document.createElement('span')
-
-    var edtBtn = document.createElement('button')
-    var edtText = document.createTextNode('Edit')
-    edtBtn.setAttribute('onclick','edtbtn(this)');
-    edtBtn.appendChild(edtText);
-    
-    span.appendChild(edtBtn)
-    
-    
-    var dltBtn = document.createElement('button')
-    var dltText = document.createTextNode('Delete')
-    dltBtn.setAttribute('onclick','dltbtn(this)');
-    
-    dltBtn.appendChild(dltText);
-    span.appendChild(dltBtn)
-    
-    li.appendChild(span)
-    
-    
 }
 }
 
@@ -47,16 +69,24 @@ function addTodo() {
 function dltAll() {
     document.getElementById('todoList').innerHTML = '';
     document.getElementById('inputTodo').value = '';
+    firebase.database().ref('/todo').remove()
 
 }
 
 function edtbtn(e){
-console.log(e.parentNode.parentNode.firstChild.nodeValue)
 var text = prompt('Edit Task', e.parentNode.parentNode.firstChild.nodeValue);
 e.parentNode.parentNode.firstChild.nodeValue = text;
+obj = {
+    key : e.id,
+    todo : text
+}
+firebase.database().ref('/todo/'+ e.id).set(obj);
 }
 
 
 function dltbtn(e){
     e.parentNode.parentNode.remove()
+    
+    firebase.database().ref('/todo/'+ e.id).remove();
+    console.log(e)
 }
